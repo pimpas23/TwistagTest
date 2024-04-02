16bb50cfa8d0bd8635f7d7b5b3c4bbd0ae2f4462
pimparel <1brunofpgoncalves1@gmail.com>
2024-04-02 09:22:05 +0100

    overview still with problems

---
+++ src/TwistagTest.App/Components/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,15 @@
+﻿using TwistagTest.App.Models;
+using TwistagTest.Shared.Models;
+
+namespace TwistagTest.App.Components.Pages
+{
+    public partial class EmployeeOverview
+    {
+        public List<Employee> Employees { get; set; } = default!;
+
+        //protected override void OnInitialized()
+        //{
+        //    Employees = MockDataService.Employees;
+        //}
+    }
+}
