b57274948054e98b8a46a5b4f2314fd58c0ce5ac
pimparel <1brunofpgoncalves1@gmail.com>
2024-04-03 14:55:02 +0100

    add razor.cs files missing

---
+++ src/TwistagTest.App/Components/EmployeeCard.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using TwistagTest.Shared.Models;
+
+namespace TwistagTest.App.Components
+{
+    public partial class EmployeeCard
+    {
+        [Parameter]
+        public Employee Employee { get; set; } = default!;
+
+        [Parameter]
+        public EventCallback<Employee> EmployeeQuickViewClicked { get; set; }
+    }
+}
---
+++ src/TwistagTest.App/Components/EmployeeOverview.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using TwistagTest.App.Services;
+using TwistagTest.Shared.Models;
+
+namespace TwistagTest.App.Components
+{
+    public partial class EmployeeOverview
+    {
+        [Inject]
+        public IEmployeeDataService? employeeDataService { get; set; }
+        public List<Employee>? Employees { get; set; } = default!;
+        private Employee? _selectedEmployee;
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employees = (await employeeDataService?.GetAllEmployees()).ToList();
+        }
+
+        public void ShowQuickViewPopup(Employee selectedEmployee)
+        {
+            _selectedEmployee = selectedEmployee;
+        }
+    }
+}
